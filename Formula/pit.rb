35c05af5ffad5ebec19eaab8b21a5f01026a15be
Ilia Aphtsiauri <ilia@phoebe.ai>
2026-02-19 22:40:32 +0200

    pit 0.3.0

--- Formula/pit.rb
+++ Formula/pit.rb
@@ -1,18 +1,18 @@
 class Pit < Formula
   desc "Run multiple coding agents in parallel with git worktree isolation"
   homepage "https://github.com/iaptsiauri/pit"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/iaptsiauri/pit/releases/download/v0.2.1/pit-0.2.1-aarch64-apple-darwin.tar.gz"
-      sha256 "a8ec276260123a97aa12bae89c844b649a6f266dbfc38992d927979eeacda7ed"
+      url "https://github.com/iaptsiauri/pit/releases/download/v0.3.0/pit-0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "8338f284dd5b80efaa91064d87bc9fbf0fb79996020184eab1b5c48a3a67afc6"
     end
 
     on_intel do
-      url "https://github.com/iaptsiauri/pit/releases/download/v0.2.1/pit-0.2.1-x86_64-apple-darwin.tar.gz"
-      sha256 "a0450d50339f8875a3960d01fd90c53fb654089307a31af86c80baeb55cd1620"
+      url "https://github.com/iaptsiauri/pit/releases/download/v0.3.0/pit-0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "02244456a0122b8dbf60b3ceae2dad8d69243dc30fb3d9df1281d4adfbac0952"
     end
   end
 
